cd4096753453fd934dfdf8c7eadf11d640e50a1c
breeendon <152670789+breeendon@users.noreply.github.com>
2025-08-04 14:27:38 -0500

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f1655cffcaf2b6723080a03f5c35ae05
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f3fe69aaac77ec4f9093e3fec5235770",
+"flutter_bootstrap.js": "b20a8246b9f780154ef722be61c655ba",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
